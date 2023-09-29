1eed61fd2c257d95a8470b2bd8d64429f48d07d1
Victor Escandiel <joaoescandielb@gmail.com>
2023-09-29 17:45:52 -0300

    printing the selected candidates

--- flux-control/src/controlcandidates/candidature/SelectiveProcess.java
+++ flux-control/src/controlcandidates/candidature/SelectiveProcess.java
@@ -1,19 +1,47 @@
 package controlcandidates.candidature;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class SelectiveProcess {
     public static void main(String [] args) {
-        analyzeCandidate(1900.0);
-        analyzeCandidate(2200.0);
-        analyzeCandidate(2000.0);
+        printSelected();
+        SelectionCandidates();
+
     }
-    static void analyzeCandidate(double intendedSalary) {
+
+    static void printSelected() {
+        String [] candidates = {"FELIPE", "MARCIA", "JULIA", "PAULO", "AUGUSTO"};
+        System.out.println("Printing the candidates list informing the index of the element");
+        for(int i=0; i < candidates.length; i++){
+            System.out.println("The candidate of number "+ (i+1) + "is " + candidates[i]);
+        }
+
+
+    }
+
+    static void SelectionCandidates() {
+        String [] candidates = {"FELIPE", "MARCIA", "JULIA", "PAULO", "AUGUSTO", "MONICA", "FABRICIO", "MIRELA", "DANIELA", "JORGE"};
+        int selectedCandidates = 0;
+        int actualCandidate = 0;
         double baseSalary = 2000.0;
-        if(baseSalary > intendedSalary) {
-            System.out.println("CALL TO THE CANDIDATE");
-        }else if(baseSalary == intendedSalary) {
-            System.out.println("CALL THE CANDIDATE WITH A COUNTER PROPOSAL");
-        }else {
-            System.out.println("WAITING THE RESULT FOR THE REST OF THE CANDIDATES");
+          
+        while(selectedCandidates < 5 && actualCandidate < candidates.length) {
+            
+            String candidate = candidates[actualCandidate];
+            double intendedSalary = intendedValue();
+            System.out.println("THE CANDIDATE " + candidate + " REQUESTED THIS SALARY VALUE " + intendedSalary);
+
+
+            if(baseSalary >= intendedSalary) {
+                System.out.println("THE CANDIDATE " + candidate + " WAS REQUESTED TO THE JOB");
+                selectedCandidates++;
+            }
+
+            actualCandidate++;
         }
     }
+
+    static double intendedValue() {
+        return ThreadLocalRandom.current().nextDouble(1800, 2200);
+    }
+
 }

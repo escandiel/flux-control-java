08dc124fb5e67e71274dabb828b3c911ad7a1669
Victor Escandiel <joaoescandielb@gmail.com>
2023-09-29 16:38:53 -0300

    Creating method analyzeCandidate

--- flux-control/src/controlcandidates/candidature/SelectiveProcess.java
+++ flux-control/src/controlcandidates/candidature/SelectiveProcess.java
@@ -4,4 +4,7 @@ public class SelectiveProcess {
     public static void main(String [] args) {
         System.out.println("Processo seletivo");
     }
+    static void analyzeCandidate(double intendedSalary) {
+        double baseSalary = 2000.0;
+    }
 }

7a081d59c094a1e78d20c5c29eb8a3ff2c8f493c
Victor Escandiel <joaoescandielb@gmail.com>
2023-09-29 16:51:21 -0300

    Simple Algorithm to select candidates

--- flux-control/src/controlcandidates/candidature/SelectiveProcess.java
+++ flux-control/src/controlcandidates/candidature/SelectiveProcess.java
@@ -2,9 +2,18 @@ package controlcandidates.candidature;
 
 public class SelectiveProcess {
     public static void main(String [] args) {
-        System.out.println("Processo seletivo");
+        analyzeCandidate(1900.0);
+        analyzeCandidate(2200.0);
+        analyzeCandidate(2000.0);
     }
     static void analyzeCandidate(double intendedSalary) {
         double baseSalary = 2000.0;
+        if(baseSalary > intendedSalary) {
+            System.out.println("CALL TO THE CANDIDATE");
+        }else if(baseSalary == intendedSalary) {
+            System.out.println("CALL THE CANDIDATE WITH A COUNTER PROPOSAL");
+        }else {
+            System.out.println("WAITING THE RESULT FOR THE REST OF THE CANDIDATES");
+        }
     }
 }

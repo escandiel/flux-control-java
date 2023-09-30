9d6cd8e56374c9c5f1ee74360a86f63826db180a
Victor Escandiel <joaoescandielb@gmail.com>
2023-09-30 13:49:21 -0300

    creating the counter challenge

---
+++ flux-control/src/controlcandidates/candidature/InvalidParametersException.java
@@ -0,0 +1,8 @@
+package controlcandidates.candidature;
+
+public class InvalidParametersException extends Exception {
+    public InvalidParametersException(String message) {
+        super(message);
+    }
+    
+}
